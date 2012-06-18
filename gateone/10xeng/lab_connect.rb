a153939d331dc2a94ef17e80dbf831753b60e206
Radim Marek <radim@laststation.net>
2012-06-18 21:15:32 +0200

    Lab connector build environment for GateOne's ssh_connect

--- gateone/10xeng/lab_connect.rb
+++ gateone/10xeng/lab_connect.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'yajl'
+require 'highline/import'
 
 puts "***"
 puts "*** 10xEngineer.me lab connector"
@@ -10,11 +11,43 @@ puts
 session_file = File.join(ENV["GO_USER_DIR"], ENV["GO_USER"], "session")
 
 if File.exists?(session_file)
-  puts "Using #{session_file}"
-
   session = Yajl::Parser.parse(IO.read(session_file))
 
-  puts session.inspect
+  puts "Lab environment"
+  puts
+  session["lab"]["vm"].each do |vm|
+    puts "\t#{vm["alias"]}\t#{vm["ip_addr"]}\t#{vm["id"]}"
+  end
+  puts
+
+  # TODO support vm_id passed using command line parameter
+  vm_list = session["lab"]["vm"].map {|vm| vm["alias"]}
+
+  target = ask("server? ") {|q| q.default = vm_list.first}
+
+  unless vm_list.include?(target)
+    puts "Invalid server!"
+    sleep 10
+    exit
+  end
+
+  cmd_parts = ["/opt/gateone/plugins/ssh/scripts/ssh_connect.py"]
+  cmd_parts = cmd_parts + ARGV
+
+  # TODO hardcoded user/port
+  vm = session["lab"]["vm"].select {|vm| vm["alias"] == target}
+  vm = vm.first
+
+  cmd_parts << "ssh://ubuntu@#{vm["ip_addr"]}"
+
+  cmd = cmd_parts.join(" ")
+
+  exec cmd
+
+  sleep 60
+else
+  puts "WARNING: session file not found! Try to logout."
+
+  ask("ready?") {|q| q.default = "y"}
 end
 
-exec "irb"
